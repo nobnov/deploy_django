70197b29d2864fe260c4238bc7e61c999fbeef9b
Nobukazu Ishii <nobnov@users.noreply.github.com>
2021-05-25 22:35:17 +0900

    added url

--- deploy_django/urls.py
+++ deploy_django/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import firstapp.views as firstapp
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', firstapp.results, name='home'),
+
 ]
